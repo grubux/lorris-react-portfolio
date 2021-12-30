48916c06e1ef43ba69042ea0e533a7dc59ddd38e
Lorris Lehuede <lehuedelorris@gmail.com>
2021-12-30 08:16:35 +0100

    Updates

---
+++ precache-manifest.c46f89f89060587e8ad6e53ecbea430c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71f87ea53e254c099b40f80b040ad98f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "105480a719ec434212c5",
+    "url": "./static/css/2.9c389199.chunk.css"
+  },
+  {
+    "revision": "cf6106048b09fd077ad8",
+    "url": "./static/css/main.4588be86.chunk.css"
+  },
+  {
+    "revision": "105480a719ec434212c5",
+    "url": "./static/js/2.d77ee59d.chunk.js"
+  },
+  {
+    "revision": "f38308786670e030dbb024eae0a4f8b8",
+    "url": "./static/js/2.d77ee59d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf6106048b09fd077ad8",
+    "url": "./static/js/main.9418525e.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  },
+  {
+    "revision": "63270399fe72c175ba89c90ec903dc4b",
+    "url": "./static/media/mainLogo.63270399.png"
+  }
+]);
